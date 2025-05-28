34adcece4abe1875f7aff0c98b16fb304c63881e
unknown <ferdy.fermadi99@gmail.com>
2025-05-28 21:47:10 +0700

    make a middleware for detail of each HTTP request

---
+++ middleware/logger.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		duration := time.Since(start)
+
+		log.Printf("Method: %s, URL: %s, Duration: %s", r.Method, r.URL.Path, duration)
+	})
+}
